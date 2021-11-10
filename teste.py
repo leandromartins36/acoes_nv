28d894f4d71521c733c01bfba01af8ce8b80693c
leandro de menezes martins <leandro.m.martins.info@gmail.com>
2021-11-10 14:01:57 -0300

    capturando os dados da infomoney
    Capturado os dados e append no DataFrame.

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import requests as rs
-# from IPython.display import HTML
+import numpy as np
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup as bs
 from urllib.error import URLError, HTTPError
@@ -9,31 +9,32 @@ from urllib.request import urlopen, Request
 import ssl
 from selenium import webdriver
 from selenium.webdriver import Firefox
+import time
 
 navegador = webdriver.Firefox(executable_path='./geckodriver.exe')
 navegador.maximize_window()
-
 agente = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
 
-url = 'https://www.infomoney.com.br/cotacoes/fundo-imobiliario-cxco11/historico/'
+# url = 'https://www.infomoney.com.br/cotacoes/fundo-imobiliario-cxco11/historico/'
 
 headers = {'User-Agent': agente}
-gcontext = ssl.SSLContext()
+#gcontext = ssl.SSLContext()
 
-navegador.get(url)
+empresas_df = pd.read_excel('EmpresasInfomoney.xlsx')
+url = "https://www.infomoney.com.br/cotacoes/"
 
-
-
-#info = urllib.request.urlopen(url, context=gcontext).read()
-info = rs.get(url, headers=headers)
-
-html = bs(info.text, 'html.parser')
-
-# data_acao, fechamento = [],[]
-html = html.findAll('table', id = 'quotes_history')
-''' data_acao.append(html[0].text)
-fechamento.append(html[2].text) '''
-
-df = pd.DataFrame(html)
-
-print(df)
+contador = 0
+while (contador < len(empresas_df)):
+    for empresa in empresas_df["Empresas"]:
+        url_nv = ''.join([url,empresa,'/historico/'])
+        #print(empresa)
+        navegador.get(url_nv)
+        time.sleep(5)
+        conteudo = rs.get(url_nv, headers=headers)
+        time.sleep(2)
+        tb_din = navegador.find_element_by_xpath('//*[@id="quotes_history"]').get_attribute('outerHTML')#encontra o elemento da pagina e grava na memoria os dados da tabela.
+        pd_html = pd.read_html(tb_din, decimal = ',', thousands = '.', index_col="DATA") 
+        time.sleep(2)
+        contador +=1  
+        time.sleep(3)  
+        print(pd_html[0].head())
